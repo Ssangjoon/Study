989fb623588f49765c77feb24f6f4aa4a8790c8e
youcanrun <qkqh34k@gmail.com>
2022-03-07 02:59:41 +0900

    .

--- src/BackJoon/BinarySearch.java
+++ src/BackJoon/BinarySearch.java
@@ -1,5 +1,11 @@
 package BackJoon;
 
-public class BinarySearch {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class BinarySearch {
+  public static void main(String[] args) thorws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] N = new int[Integer.parseInt(br.readLine())];
+  }
 }
