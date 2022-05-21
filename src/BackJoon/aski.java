2b8ac404bfb542a077750c55162bca7a34577bfb
youcanrun <qkqh34k@gmail.com>
2022-05-21 23:12:05 +0900

    aki

---
+++ src/BackJoon/aski.java
@@ -0,0 +1,16 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class aski {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.next();
+
+    System.out.println(a.charAt(0)+0);
+
+  }}
+
