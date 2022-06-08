5129593f792ea2d8d803fd3ed7f0a50463ce4a3d
youcanrun <qkqh34k@gmail.com>
2022-06-08 23:09:57 +0900

    scan gugu

--- src/basic/Gugudan.java
+++ src/basic/Gugudan.java
@@ -1,12 +1,16 @@
 package basic;
 
+import java.util.Scanner;
+
 public class Gugudan {
 
   public static void main(String[] args) {
-    for (int i = 0; i< 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.println(i + "단:" + i + "x" + j + "=" + i*j);
-      }
+    System.out.println("구구단 중 출력할 단은? : ");
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+
+    for (int j = 1; j < 10; j++) {
+      System.out.println(i+"단:"+i+"x"+j+"="+i*j);
     }
   }
 
