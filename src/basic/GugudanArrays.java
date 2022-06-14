dd269c8ca25d642d8049b3bc1c8e465652b08510
youcanrun <qkqh34k@gmail.com>
2022-06-14 16:37:53 +0900

    grraygugu

---
+++ src/basic/GugudanArrays.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Arrays;
+
+public class GugudanArrays {
+
+  public static void main(String[] args) {
+    int[] result = new int[9];
+    for (int i=1; i<=result.length; i++) {
+      for (int j=1; j<=result.length; j++) {
+        result[j-1] = i*j;
+      }
+      System.out.println(i + "단: " + Arrays.toString(result));
+    }   
+  }
+}
