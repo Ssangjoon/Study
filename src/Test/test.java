25c3175b78be6ba253db80fa5ca2f26c3d99a14c
youcanrun <qkqh34k@gmail.com>
2022-06-15 20:51:44 +0900

    review package gradle

--- src/Test/test.java
+++ src/Test/test.java
@@ -1,16 +1,9 @@
 package Test;
 
-public class test {
+public class Test {
 
   public static void main(String[] args) {
-    System.out.println((char)0x41);
-    int[] arr1 = new int[3];
-    int[] arr2;
-    arr1[0] = 200;
-    arr2 = arr1;
-    System.out.println(arr2[0]);
-    int a = 1, b=3, c=4;
-    System.out.println(a+b+c);
+    System.out.println("Hello,World!!"); 
 
   }
 
