efc302a24de10e08ea86ee47fc92232b155a8dfc
youcanrun <qkqh34k@gmail.com>
2022-08-22 23:12:34 +0900

    js review

---
+++ bin/javascript/kchEx/knn.js
@@ -0,0 +1,6 @@
+const movies = [
+    {action:12, kiss:2, cate: 'A'}, // 암살
+    {action:2, kiss:14, cate: 'M'}, // 어바웃타ㅣㅁ
+    {action:7, kiss:2, cate: 'A'}, // 토이스토리 
+    {action:2, kiss:10, cate: 'M'}, // 타이타닉
+]
--- src/Test/test.java
+++ src/Test/test.java
@@ -1,10 +1,23 @@
 package Test;
 
+import java.util.Arrays;
+
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Hello,World!!4"); 
+    Integer[] arr = {1,2,3,4,5,35,2523,8,24};
+    //    Arrays.sort(arr); 
+    int target = 40;
 
+    //        Arrays.sort(arr, (a, b) -> {
+    //          int gap1 = Math.abs(target-a);
+    //          int gap2 = Math.abs(target-b);
+    //          return gap1>gap2 ? 1 : -1;
+    //        });
+
+    Arrays.sort(arr, (a, b) -> Math.abs(target - a)> Math.abs(target - b) ? 1: -1);
+
+    System.out.println(Arrays.toString(arr));
   }
 
 }
