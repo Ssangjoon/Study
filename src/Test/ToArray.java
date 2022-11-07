e3a14276c5b5d4124b17df1b591bf03bdfda52b2
youcanrun <qkqh34k@gmail.com>
2022-11-08 03:03:29 +0900

    에디터, 짜증남....

--- src/Test/ToArray.java
+++ src/Test/ToArray.java
@@ -7,8 +7,12 @@ public class ToArray {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String s = br.readLine();
-    char[] ch = s.toCharArray();
-    System.out.println(ch[0]);
+    String str = br.readLine();
+    char[] ch = str.toCharArray();
+    System.out.println("ch = " + ch);
+
+    for ( char a : str.toCharArray()){
+      System.out.println("a = " + a);
+    }
   }
 }
--- src/baekjoon/MainEditor.java
+++ src/baekjoon/MainEditor.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class MainEditor {
+    // 맞는거 같은데 결과가 자꾸 달라서 보니 case에 break를 붙이지 않아서 그런거 였다 .. 촴..
+    // 아니 근데 두번쨰 예제는 답이 안나옴
     static Stack<Character> stk = new Stack<>();
     static Stack<Character> stk2 = new Stack<>();
     static int cursor;
@@ -16,12 +18,14 @@ public class MainEditor {
 
         StringTokenizer st;
 
-        String S = br.readLine();
-        cursor = S.length()+1;
-        for (char s:S.toCharArray()){
+        String str = br.readLine();
+        // 맨오른쪽에 커서는 위치한다.
+        cursor = str.length()+1;
+        for (char s:str.toCharArray()){
             stk.push(s);
         }
 
+        // 명령어 개수
         int N = Integer.parseInt(br.readLine());
         while(N-- > 0) {
             st = new StringTokenizer(br.readLine());
@@ -31,33 +35,47 @@ public class MainEditor {
                     break;
                 case "L":
                     L();
+                    break;
                 case "D":
                     D();
+                    break;
                 case "B":
                     B();
+                    break;
+                default:
+                    System.out.println(cursor);
+                    System.out.println(stk.toString());
             }
         }
-        System.out.println(stk.toString());
+        System.out.println("result => " + stk.toString());
     }
     static void L(){
-        if(cursor > 0)
+        // 커서를 왼쪽으로 한칸 (맨앞이면 무시)
+        if(cursor >= 1)
         cursor--;
     }
     static void D(){
-        if (cursor < stk.size()+2)
+        // 커서를 오른쪽으로 한칸 (맨 뒤면 무시)
+        if (cursor <= stk.size())
         cursor++;
     }
     static void B(){
-        int len = stk.size() - (cursor - 1);
-        for (int i = 0; i < len; i++) {
-            stk2.push(stk.pop());
-        }
-        stk.remove(cursor-1);
-        for (int i = 0; i < len; i++) {
-            stk.push(stk2.pop());
+        // 커서 왼쪽에 있는 문자를 삭제함 (맨 앞이면 무시)
+        // 삭제로 인해 커서는 왼쪽으로 이동 한듯 보이나 , 실제로 커서 오른쪽에 있던 문자는 그대로
+        if (cursor != 0){
+            // pop할 횟수
+            int len = stk.size() - (cursor - 1);
+            for (int i = 0; i < len; i++) {
+                stk2.push(stk.pop());
+            }
+            stk.remove(cursor-2);
+            for (int i = 0; i < len; i++) {
+                stk.push(stk2.pop());
+            }
         }
     }
     static void P(Character x){
+        // x를 커서 왼쪽에 추가
         int len = stk.size() - cursor - 1;
         for (int i = 0; i < len; i++) {
             stk2.push(stk.pop());
